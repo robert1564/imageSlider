450b487e469cd6b0e66d32e12bc477e5b4b7c882
Robert <robertmaximus383@gmail.com>
2023-08-13 15:13:16 +0300

    progress but still bug

--- src/app/page.js
+++ src/app/page.js
@@ -88,21 +88,21 @@ export default function SlidingImages() {
     target: container,
     offset: ["start end", "end start"],
   });
+
   const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
 
   const sliderFirst = useRef(null);
-  const sliderSecond = useRef(null);
   let xPercent = 0;
   let xPercent2 = 0;
 
-  let direction = -1;
+  let direction = 1;
 
   useEffect( () => {
     gsap.registerPlugin(ScrollTrigger);
     gsap.to(sliderFirst.current, {
       scrollTrigger: {
         trigger: document.documentElement,
-        scrub: 0.25,
+        scrub: 1,
         start: 0,
         end: window.innerHeight,
         onUpdate: e => direction = e.direction * -1
@@ -110,17 +110,6 @@ export default function SlidingImages() {
       x: "-500px",
     })
     requestAnimationFrame(animate);
-    gsap.to(sliderSecond.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: 0.25,
-        start: 0,
-        end: window.innerHeight,
-        onUpdate: e => direction = e.direction * -1
-      },
-      x: "500px",
-    })
-    requestAnimationFrame(animate2);
   }, [])
 
   const animate = () => {
@@ -130,22 +119,18 @@ export default function SlidingImages() {
     else if(xPercent > 0){
       xPercent = -100;
     }
-    gsap.set(slider1.current, {xPercent: xPercent})
-    gsap.set(slider2.current, {xPercent: xPercent})
-    requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  }
-
-  const animate2 = () => {
     if(xPercent2 < -100){
       xPercent2 = 0;
     }
     else if(xPercent2 > 0){
       xPercent2 = -100;
     }
+    gsap.set(slider1.current, {xPercent: xPercent})
+    gsap.set(slider2.current, {xPercent: xPercent})
     gsap.set(slider3.current, {xPercent: xPercent2})
     gsap.set(slider4.current, {xPercent: xPercent2})
-    requestAnimationFrame(animate2);
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
     xPercent2 += -0.1 * direction;
   }
 
@@ -173,7 +158,7 @@ export default function SlidingImages() {
         })}
       </motion.div>
       </div>
-      <div ref={slider2}>
+      <div ref={slider2} className={styles.sliderSec}>
         <motion.div className={styles.slider}>
         {slider1.map((project, index) => {
           return (
@@ -195,7 +180,7 @@ export default function SlidingImages() {
       </motion.div>
       </div>
       </div>
-      <div ref={sliderSecond} className={styles.slider}>
+      <div ref={sliderFirst} className={styles.slider}>
       <div ref={slider3}>
       <motion.div className={styles.slider}>
         {slider2.map((project, index) => {
@@ -217,7 +202,7 @@ export default function SlidingImages() {
         })}
       </motion.div>
       </div>
-      <div ref={slider4}>
+      <div ref={slider4} className={styles.sliderSec}>
         <motion.div className={styles.slider}>
         {slider2.map((project, index) => {
           return (
